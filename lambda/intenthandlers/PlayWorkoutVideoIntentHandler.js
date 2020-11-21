81919b9b709552949d8d167cd1ad8f83b14eec8f
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2020-11-21 00:32:06 -0800

    Remove some funky prompts.

--- lambda/intenthandlers/PlayWorkoutVideoIntentHandler.js
+++ lambda/intenthandlers/PlayWorkoutVideoIntentHandler.js
@@ -5,23 +5,23 @@ const ytdl = require('ytdl-core');
 const doc = require("response/display/WorkoutVideoView/document.json");
 const workoutVideosDataSource = require("response/display/WorkoutVideoView/datasources/default");
 
-module.exports = PlayWorkoutVideoIntentHandler = {
+const videos = [
+  "https://www.youtube.com/watch?v=-5ztdzyQkSQ",
+  "https://www.youtube.com/watch?v=Mvo2snJGhtM",
+];
 
+module.exports = PlayWorkoutVideoIntentHandler = {
   canHandle(handlerInput) {
     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
       && Alexa.getIntentName(handlerInput.requestEnvelope) === 'PlayWorkoutVideoIntent';
   },
   async handle(handlerInput) {
-    const videos = [
-      "https://www.youtube.com/watch?v=-5ztdzyQkSQ",
-      "https://www.youtube.com/watch?v=Mvo2snJGhtM",
-    ];
 
     let info = await ytdl.getInfo(videos[Math.random() < 0.5 ? 0 : 1]);
     let highQualityAudioVideoStream = ytdl.chooseFormat(info.formats, { filter: 'audioandvideo', quality: 'highestvideo' });
-    console.log('Requested Quality', highQualityAudioVideoStream.url);
+    console.log('URL to be played', highQualityAudioVideoStream.url);
 
-    const speakOutput = 'Here is a workout video for you. Edited.';
+    const speakOutput = 'Here is a workout video for you.';
     const aplDirective = {
       type: "Alexa.Presentation.APL.RenderDocument",
       version: "1.4",
