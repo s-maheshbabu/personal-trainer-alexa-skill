7c97b11dd4ad5c08ce8f3df35956ccafae408870
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2020-11-25 09:40:52 -0800

    Added tests for edge cases while searching for videos.

--- lambda/test/data/VideoIndex.test.ts
+++ lambda/test/data/VideoIndex.test.ts
@@ -42,12 +42,32 @@ describe("Search for videos in the repository", () => {
         expect(urls).to.eql(expectedUrls);
     });
 
+    it('should return an empty array when no matching workouts are found.', async () => {
+        const urls = await search('STRETCHING', 17, ['GLUTES'], 'HARD');
+
+        assert(Array.isArray(urls));
+        expect(urls.length).to.equal(0);
+    });
+
     it('should throw an error is exercise type is not provided.', async () => {
         try {
             await search(undefined,);
-            fail('Expected an error to be thrown but no errors where thrown.')
+            fail('Expected an error to be thrown because the exersice type was missing but no errors where thrown.')
         } catch (error) {
             expect(error.message).to.eql('Exercise Type is a required argument.');
+        }
+    });
+
+    it('should throw an error if muscle groups is provided but is not a valid non-empty array.', async () => {
+        const invalidMuscleGroupsInputs = ['not an array', []];
+        for (let i = 0; i < invalidMuscleGroupsInputs.length; i++) {
+            const invalidMuscleGroupsInput = invalidMuscleGroupsInputs[i];
+            try {
+                await search('STRENGTH_TRAINING', undefined, invalidMuscleGroupsInput, undefined);
+                fail('Expected an error to be thrown because the muscle grousp input is invalid but no errors where thrown.')
+            } catch (error) {
+                expect(error.message).to.eql(`Invalid muscle groups input: ${invalidMuscleGroupsInput}`);
+            }
         }
     });
 });
