23e981223232cbf0bd7b87f6c600a4bf522698d4
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2020-11-25 17:57:33 -0800

    Create an entry point for Alexa hosted skills.
    Create an entry point to satisfy Alexa's requirements without changing the code structure.
    Alexa cannot be configured to invoke lambda/src/index and needs that the index file is at lambda/index.

--- lambda/index.js
+++ lambda/index.js
@@ -1,31 +1,6 @@
-const Alexa = require('ask-sdk-core');
-
-/* *
- * The intent reflector is used for interaction model testing and debugging.
- * It will simply repeat the intent the user said. You can create custom handlers for your intents 
- * by defining them above, then also adding them to the request handler chain below 
- * */
-const IntentReflectorHandler = {
-    canHandle(handlerInput) {
-        return true;
-    },
-    handle(handlerInput) {
-        const speakOutput = `You just triggered an intent`;
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .getResponse();
-    }
-};
-
 /**
- * This handler acts as the entry point for your skill, routing all request and response
- * payloads to the handlers above. Make sure any new handlers or interceptors you've
- * defined are included below. The order matters - they're processed top to bottom 
+* Create an entry point to satisfy Alexa's requirements without changing the code structure.
+* Alexa-hosted skills cannot be configured to invoke lambda/src/index and require that the index
+* file be at lambda/index. Hence this entry point which immediately routes to the actual index.
  * */
-exports.handler = Alexa.SkillBuilders.custom()
-    .addRequestHandlers(
-        IntentReflectorHandler,
-    )
-    .withCustomUserAgent('sample/personal-trainer/v1.2')
-    .lambda();
+exports.handler = require('./src/index').handler;
