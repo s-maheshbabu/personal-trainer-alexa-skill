d463afe8397e757f0ddfd8545c0d98d2d07eb4d6
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2020-11-25 23:10:53 -0800

    Documentation update.

--- lambda/index.js
+++ lambda/index.js
@@ -1,6 +1,7 @@
 /**
 * Create an entry point to satisfy Alexa's requirements without changing the code structure.
 * Alexa-hosted skills cannot be configured to invoke lambda/src/index and require that the index
-* file be at lambda/index. Hence this entry point which immediately routes to the actual index.
+* file be at lambda/index. Hence this entry point which immediately routes to the actual entry
+* point of the code.
  * */
 exports.handler = require('./src/index').handler;
