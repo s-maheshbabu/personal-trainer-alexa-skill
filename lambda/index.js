1ff3fbb0d6af1d5df36323800f8d3a339ff6f788
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2020-11-25 17:33:31 -0800

    Add a skill entry file for Alexa hosted skills.

---
+++ lambda/index.js
@@ -0,0 +1,38 @@
+const Alexa = require('ask-sdk-core');
+
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        IntentReflectorHandler,
+    )
+    .addRequestInterceptors(
+        LogRequestInterceptor,
+    )
+    .addResponseInterceptors(
+        LogResponseInterceptor,
+    )
+    .withCustomUserAgent('sample/personal-trainer/v1.2')
+    .lambda();
