4abbb51c4e2263d95b91b77b25aeb090d57c5eb9
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2020-11-25 23:33:08 -0800

    Enable grunt based deployments to Lambda.

--- lambda/Gruntfile.js
+++ lambda/Gruntfile.js
@@ -10,6 +10,22 @@ grunt.initConfig({
       }
     }
   },
+  lambda_deploy: {
+    default: {
+      options: {
+        aliases: "beta",
+        enableVersioning: true
+      },
+      arn: "arn:aws:lambda:us-east-1:837603326872:function:personal-trainer-alexa-skill"
+    },
+    prod: {
+      options: {
+        aliases: "prod",
+        enableVersioning: true
+      },
+      arn: "arn:aws:lambda:us-east-1:837603326872:function:personal-trainer-alexa-skill"
+    }
+  },
   lambda_package: {
     default: {},
     prod: {}
