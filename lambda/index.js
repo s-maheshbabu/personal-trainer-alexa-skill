7d0fd3254984a080a7558864256765454a0b2ed6
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2020-11-25 23:24:36 -0800

    Moved repo to github and so getting rid all Alexa-Hosted quirks.

--- lambda/index.js
+++
@@ -1,7 +0,0 @@
-/**
-* Create an entry point to satisfy Alexa's requirements without changing the code structure.
-* Alexa-hosted skills cannot be configured to invoke lambda/src/index and require that the index
-* file be at lambda/index. Hence this entry point which immediately routes to the actual entry
-* point of the code.
- * */
-exports.handler = require('./src/index').handler;
